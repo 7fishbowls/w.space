c7c126609a108c3e208234cedf2b7bbc3cdf074d
Shaik Ali <7fishbowls@gmail.com>
2025-01-04 07:44:16 -0800

    OMG:

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import styles from "./page.module.css";
+import { FaAngleDown } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <section className={styles.main}>
       <p>W.SPACE</p>
+      <h4>
+        <FaAngleDown size={30} />
+      </h4>
     </section>
   );
 }
